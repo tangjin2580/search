4504ce52e1402e6eac08272b818b76d77108691a
李鑫 <2324099478@qq.com>
2022-08-29 18:37:29 +0800

    music

---
+++ static/js/v3.js
@@ -0,0 +1,4 @@
+const windowWidth = $(window).width();
+const meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
+console.log('\n' + ' %c tangjinme ' + ' %c http://www.tangjinme.cn/ ' + '\n' + '\n',
+    'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
